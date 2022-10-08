1cf80bd7b34595a61be7792d4aeda41d6dc6206d
Jonathan Sugihara <jonathansugihara@Jonathans-MacBook-Pro.local>
2022-10-07 22:21:54 -0600

    databse.js update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,47 +1,48 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const init_DB = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('oops! the database jate already exists');
         return;
       }
+
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('the database jate is created');
     },
+
   });
 
 export const putDb = async (id, content) => {
-  console.log('PUT to the database');
+  console.log('API - PUT to the database: jate');
 
   const jateDb = await openDB('jate', 1);
-
   const tx = jateDb.transaction('jate', 'readwrite');
-
   const store = tx.objectStore('jate');
   const request = store.put({ id: id, content: content });
 
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data is now saved to the database', result);
 };
 
+
+
 export const getDb = async () => {
-  
-  console.log('GET from the database');
+
+  console.log('API - GET from the database: jate');
 
   const jateDb = await openDB('jate', 1);
-
   const tx = jateDb.transaction('jate', 'readonly');
-
-
-
   const store = tx.objectStore('jate');
-
   const request = store.getAll();
 
   const result = await request;
   console.log('result.value', result);
 };
 
-initdb();
+
+//run function
+
+init_DB();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,37 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+//complete
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+
+  butInstall.classList.toggle("hidden", false)
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//complete
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+
+
+  butInstall.classList.toggle("hidden", true);
+
+});
+
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+//complete
+window.addEventListener('appinstalled', (event) => {
+
+window.deferredPrompt = null;
+});
