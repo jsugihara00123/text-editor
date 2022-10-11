b3c43e53e1811f29c70d37988ef9767d5042dec6
Jonathan Sugihara <jonathansugihara@Jonathans-MacBook-Pro.local>
2022-10-10 19:27:27 -0600

    webpack and src-sw update and build out

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,57 +1,79 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
+
   cacheName: 'page-cache',
   plugins: [
 
     new CacheableResponsePlugin({
       statuses: [0, 200],
+
     }),
 
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
 
+    }),
   ],
-  
 });
 
 warmStrategyCache({
+
   urls: ['/index.html', '/'],
   strategy: pageCache,
-});
 
+});
 
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 //complete
-registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  
-  
-  // staleWhileRevalidate
 
-  new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
+registerRoute(
+
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new CacheFirst({
+
+    cacheName: 'jate-asset-cache',
     plugins: [
-      
+
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      
+
+      }),
+    ],
+  })
+);
+
+// TODO: Implement asset caching
+// Complete
+
+registerRoute(
+
+  ({ request }) => request.destination === 'image',
+
+  new CacheFirst({
+
+    cacheName: 'jate-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 24 * 30, 
       }),
 
     ],
-
-  })
-
+  }),
 );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,101 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// Complete
+
 // TODO: Add CSS loaders and babel to webpack.
+// Complete
+
 
 module.exports = () => {
+
   return {
+
     mode: 'development',
     entry: {
+
       main: './src/js/index.js',
       install: './src/js/install.js'
+
     },
+
     output: {
+
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+
     },
+
+    // <-------- New Addition ----------->
+
     plugins: [
-      
+
+      new HtmlWebpackPlugin({
+
+        template: './index.html', 
+        title: 'Text Editor' 
+
+      }),
+     
+      new InjectManifest({
+
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+
+      }),
+
+      new WebpackPwaManifest({  
+
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        display: "standalone",
+        short_name: 'Editor',
+        description: 'Just Another Text Editor',
+        background_color: '#272822',
+        theme_color: '#31A9E1',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,  
+          use: ['style-loader', 'css-loader'], 
+        },
+
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, 
+          type: 'asset/resource'
+        },
+
+        {
+          test: /\.m?js$/,  
+          exclude: /(node_modules|bower_components)/,  
+          use: 
+            {
+            loader: 'babel-loader', 
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
+
+// <-------- New Addition ----------- >
+
       ],
     },
   };
